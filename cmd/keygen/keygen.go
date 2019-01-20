4678e25792351ea8c8d15889202770c5463dc7e6
regnull <regnull@gmail.com>
2019-01-19 23:03:15 -0500

    password encryption

--- cmd/keygen/keygen.go
+++ cmd/keygen/keygen.go
@@ -10,6 +10,8 @@ import (
 	"crypto/rsa"
 
 	"encoding/gob"
+
+	"github.com/regnull/raven/util"
 )
 
 func main() {
@@ -28,6 +30,7 @@ func main() {
 	}
 
 	// Generate the key.
+	fmt.Printf("generating...\n")
 	reader := rand.Reader
 	key, err := rsa.GenerateKey(reader, keySize)
 	if err != nil {
@@ -40,11 +43,25 @@ func main() {
 	encoder := gob.NewEncoder(&buf)
 	err = encoder.Encode(key)
 	if err != nil {
-		fmt.Printf("%s\n", err)
+		fmt.Printf("encoding failed: %s\n", err)
 		os.Exit(1)
 	}
 
+	// Encrypt with password.
 	password, err := util.ReadPassword()
+	fmt.Printf("\n")
+	var outputBytes []byte
+	if password != "" {
+		passwordEncryptionKey := util.KeyFromString(password)
+		outputBytes, err = util.Encrypt(buf.Bytes(), passwordEncryptionKey)
+		if err != nil {
+			fmt.Printf("encryption failed: %s\n", err)
+			os.Exit(1)
+		}
+	} else {
+		fmt.Printf("WARNING: private key is not encrypted\n")
+		outputBytes = buf.Bytes()
+	}
 
 	// Write output to the file.
 	outFile, err := os.Create(fileName)
@@ -53,13 +70,15 @@ func main() {
 		os.Exit(1)
 	}
 	defer outFile.Close()
-	n, err := outFile.Write(buf.Bytes())
+	n, err := outFile.Write(outputBytes)
 	if err != nil {
 		fmt.Printf("error writing to output file: %s\n", err)
 		os.Exit(1)
 	}
-	if n < keySize {
-		fmt.Printf("file size is too small, %d\n", n)
+	if n == 0 {
+		// This should probably never happen.
+		fmt.Printf("zero bytes written")
 		os.Exit(1)
 	}
+	fmt.Printf("key written to %s\n", fileName)
 }
