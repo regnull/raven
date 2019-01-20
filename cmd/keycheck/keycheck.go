dc1856056a9df0454da24aa10890c6d7ccddcd13
regnull <regnull@gmail.com>
2019-01-19 23:23:41 -0500

    add keycheck

---
+++ cmd/keycheck/keycheck.go
@@ -0,0 +1,80 @@
+package main
+
+import (
+	"bytes"
+	"crypto/rsa"
+	"encoding/gob"
+	"flag"
+	"fmt"
+	"os"
+
+	"github.com/regnull/raven/util"
+)
+
+const (
+	maxFileSize = 1024 * 10
+)
+
+func main() {
+	var fileName string
+	flag.StringVar(&fileName, "file", "", "key file name")
+	flag.Parse()
+
+	if fileName == "" {
+		fmt.Printf("--file must be specified")
+		os.Exit(1)
+	}
+
+	// Open file and read content.
+	fmt.Printf("reading...\n")
+	file, err := os.Open(fileName)
+	if err != nil {
+		fmt.Printf("error reading file %s, %s\n", fileName, err)
+		os.Exit(1)
+	}
+
+	buf := make([]byte, maxFileSize)
+	n, err := file.Read(buf)
+	if err != nil {
+		fmt.Printf("error reading file %s, %s\n", fileName, err)
+		os.Exit(1)
+	}
+	if n == 0 {
+		fmt.Printf("zero bytes read\n")
+		os.Exit(1)
+	}
+	buf = buf[:n]
+
+	// Password-decrypt the content.
+	fmt.Printf("asking password...\n")
+	password, err := util.ReadPassword()
+	fmt.Printf("\n")
+	var keyBytes []byte
+	if password != "" {
+		passwordEncryptionKey := util.KeyFromString(password)
+		keyBytes, err = util.Decrypt(buf, passwordEncryptionKey)
+		if err != nil {
+			fmt.Printf("decryption failed: %s\n", err)
+			os.Exit(1)
+		}
+	} else {
+		keyBytes = buf
+	}
+
+	// Deserialize and verify.
+	fmt.Printf("deserializing and verifying...\n")
+	reader := gob.NewDecoder(bytes.NewReader(keyBytes))
+	var key rsa.PrivateKey
+	err = reader.Decode(&key)
+	if err != nil {
+		fmt.Printf("error deserializing the key: %s\n", err)
+		os.Exit(1)
+	}
+
+	err = key.Validate()
+	if err != nil {
+		fmt.Printf("validation failed: %s\n", err)
+		os.Exit(1)
+	}
+	fmt.Printf("key validated\n")
+}
